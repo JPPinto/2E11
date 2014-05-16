768e8a0424c21fdf4f41dc04832738a59f95bd7a
Eduardo Fernandes <e.luis@icloud.com>
2014-05-16 10:22:45 +0100

    Whip

---
+++ 2e11/2e11/2e11.Shared/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2e11
+{
+    class Game
+    {
+        bool isWon;
+        bool isLost;
+        ushort boardSize = 4;
+
+        Tile[][] board;
+        
+
+        // Empty constructor
+        Game(){
+            initializeBoard();
+            
+
+        }
+
+        void initializeBoard() {
+            board = new Tile[boardSize][];
+
+            for (ushort i = 0; i < boardSize; i++)
+            {
+                board[i] = new Tile[boardSize];
+            }
+        }
+
+        void insertTile(Tile tile){
+
+
+        }
+
+        bool cellsAvailable(){
+            for (ushort i = 0; i < boardSize; i++){
+                for (ushort j = 0; j < boardSize; j++){
+                    if (board[i][j] == 0){
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        void addRandomTile() {
+            if (cellsAvailable()) {
+                //var value = Math.random() < 0.9 ? 2 : 4;
+                //var tile = new Tile(this.grid.randomAvailableCell(), value);
+
+                //this.grid.insertTile(tile);
+            }
+        }
+    }
+}
---
+++ 2e11/2e11/2e11.Shared/Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2e11
+{
+    class Tile
+    {
+        Position pos;
+
+        ushort value;
+
+        Tile()
+        { 
+            
+        }
+
+        int getValueRepresentation() {
+            int result = 2;
+
+            for (ushort i = value; i > 0; i++ ) {
+                result *= result;
+            }
+
+            return result;
+        }
+
+        Position getPosition() {
+            return pos;
+        }
+    }
+}
