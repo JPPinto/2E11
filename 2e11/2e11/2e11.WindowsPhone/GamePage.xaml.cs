65bea966100310e2b488bf9210d079c1044bbcb2
José Pinto <jose.vc.pinto@gmail.com>
2014-05-28 19:31:49 +0100

    Corrected grafics and grid layout.
    Correct front page image;
    Adapted to new game logic;
    Game itself at base complete.

--- 2e11/2e11/2e11.WindowsPhone/GamePage.xaml.cs
+++ 2e11/2e11/2e11.WindowsPhone/GamePage.xaml.cs
@@ -101,21 +101,23 @@ namespace _2e11
         {
             gameGrid.Children.Clear();
 
-            Tile[,] temp_board = game.getBoard();
+            ushort[,] temp_board = game.getBoard();
 
             for (ushort i = 0; i < Game.boardSize; i++)
             {
                 for (ushort j = 0; j < Game.boardSize; j++)
                 {
-                    if (!temp_board[i, j].getAvailability())
+                    if (temp_board[i, j] != 0)
                     {
-                        int tile_value = temp_board[i, j].getValue();
+                        int tile_value = temp_board[i, j];
                         if (tile_value != 0)
-                            addPiece(Game.representation[temp_board[i, j].getValue() - 1], j, i);
+                            addPiece(Game.representation[temp_board[i, j] - 1], j, i);
                     }
 
                 }
             }
+
+            //game.
         }
 
         private void addPiece(String value, int x_pos, int y_pos)
@@ -157,7 +159,7 @@ namespace _2e11
             if (!animating)
                 return;
 
-            Tile[,] temp_board = game.getBoard();
+            ushort[,] temp_board = game.getBoard();
             bool horizontal = false;
             PointerPoint pp = PointerPoint.GetCurrentPoint(e.Pointer.PointerId);
 
